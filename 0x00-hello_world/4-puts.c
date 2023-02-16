5fd7426d35f1fe733b3efd76f3fb7c06f9f941b7
christophersesugh <christohybrid185@gmail.com>
2023-02-16 22:37:24 +0000

    HW task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
