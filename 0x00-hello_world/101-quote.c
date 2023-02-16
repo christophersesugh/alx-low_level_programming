608dcc92f48961f74dfc7a82cf4a4cc2ff3680a8
christophersesugh <christohybrid185@gmail.com>
2023-02-16 23:29:28 +0000

    HW task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Retrun: 1 (Success)
+ */
+
+int main(void)
+{
+	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	return (1);
+}
