48e8564a9cb4880d5c5ccbd5b1c52392e844bd60
christophersesugh <christohybrid185@gmail.com>
2023-02-16 23:50:43 +0000

    fix 3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,9 @@
 
 int main(void)
 {
-	char sen[] = "and that piece of art is useful\" - Dora Korpar,\
-		      2015-10-19\n";
 
-	write(2, sen, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 
+		2015-10-19", 59);
+
 	return (1);
 }
