23269786803f52aef524cbdac2f2159125f98e27
christophersesugh <christohybrid185@gmail.com>
2023-02-16 23:35:10 +0000

    error fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,14 @@
 /**
  * main - Entry point
  *
- * Retrun: 1 (Success)
+ * Return: 1 (Success)
  */
 
 int main(void)
 {
-	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, sentence, 59);
+	char sen[] = "and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19\n";
+
+	write(2, sen, 59);
 	return (1);
 }
