b2c81e39e58a5e51784dbe17894799c3fd143803
christophersesugh <christohybrid185@gmail.com>
2023-02-17 23:22:40 +0000

    0x01 task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - print alphabet except q and e
+* return: 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i == 101 || i == 113)
+			continue;
+		else
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
