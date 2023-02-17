69bcbcacc7a8d8c35683016e9f733ac00aa32eba
christophersesugh <christohybrid185@gmail.com>
2023-02-17 20:50:54 +0000

    Positive or negative integer task

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Program entry point
+ * 
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n == 0)
+		printf("%d is zero", n);
+	if(n < 0)
+		printf("%d is negative", n);
+	if(n > 0)
+		printf("%d is positive", n);
+	return (0);
+}
