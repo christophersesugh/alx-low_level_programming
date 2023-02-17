6461a399ccd363f753d87b22b71adad446e9ace4
christophersesugh <christohybrid185@gmail.com>
2023-02-17 23:30:52 +0000

    0x01 task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers in base 10
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	return (0);
+}
