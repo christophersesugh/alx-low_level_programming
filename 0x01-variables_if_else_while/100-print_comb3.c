78c25b3d7a45b4f2296c962d4063f340bc17ab09
christophersesugh <christohybrid185@gmail.com>
2023-02-18 20:16:48 +0000

    0x01 adv. task 0

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Print combination of numbers from 00 to 89
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 48, j = 48;
+
+	while (i < 58)
+	{
+		while (j < 58)
+		{
+			if ((i != j) && (i < j))
+			{
+				putchar(i);
+				putchar(j);
+
+				if ((i == 56) && (j == 57))
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			j++
+		}
+		i++
+	}
+	putchar('\n');
+	return (0);
+}
