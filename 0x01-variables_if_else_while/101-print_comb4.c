8663139516c37ad1ace9d5bbb8409097512c36d6
christophersesugh <christohybrid185@gmail.com>
2023-02-18 21:19:36 +0000

    fix space error

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,24 +15,24 @@ int main(void)
 		j = 48;
 		while (j < 58)
 		{
-           	 	k = 48;
-            		while (k < 58)
-            		{
-               			if ((i != j && j != k && i != k) && (i < j && j < k && i < k))
-                		{
-                    			putchar(i);
-                   			putchar(j);
-                    			putchar(k);
+			k = 48;
+			while (k < 58)
+			{
+				if ((i != j && j != k && i != k) && (i < j && j < k && i < k))
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
 
-                    			if (i == 55 && j == 56 && k == 57)
-                    			{
-                        			break;
-                   			}
-					putchar(',');
-					putchar(' ');
-               			 }
-                		 k++;
-            		}
+						if (i == 55 && j == 56 && k == 57)
+						{
+							break;
+						}
+						putchar(',');
+						putchar(' ');
+				}
+				k++;
+			}
 			j++;
 		}
 		i++;
