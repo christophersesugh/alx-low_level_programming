891e0e309c6f3d5b45242d16b3319aa8a8df1814
christophersesugh <christohybrid185@gmail.com>
2023-02-21 10:38:48 +0000

    0x02 print alphabet x10 task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+void print_alphabet_x10();
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
+
+void print_alphabet_x10()
+{
+	int idx, char_code;
+	for (idx = 0; idx < 10; idx++)
+	{
+		for (char_code = 97; char_code < 123; char_code++)
+		{
+			_putchar(char_code);
+		}
+		_putchar('\n');
+	}
+}
