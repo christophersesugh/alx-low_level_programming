f08edae4cd9b060fad5ed6e35f016af1573f8635
christophersesugh <christohybrid185@gmail.com>
2023-02-23 16:24:25 +0000

    update: fix number bug in 0x04 task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,9 @@ void print_most_numbers(void)
 
 	for (number = 48; number < 58; number++)
 	{
-		if (number != 50 && number != 56)
-		{
-			_putchar(number);
-		}
+		if (number == 50 || number == 52)
+			continue;
+		_putchar(number);
 	}
 	_putchar('\n');
 }
