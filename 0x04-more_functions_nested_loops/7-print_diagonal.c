a61f7d44ccceff75c7c90571cc9530dc10b01cc2
christophersesugh <christohybrid185@gmail.com>
2023-02-23 22:48:51 +0000

    feat: complete 0x04 task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draw a diagonal line on the terminal
+ * @n: height of the line
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (i == j)
+				{
+					_putchar(95);
+				}
+				else
+				{
+					_putchar(' ');
+				}
+				_putchar('\n');
+			}
+		}
+	}
+}
