19006427e3bdd0bfb94ad2fbc1335534192a6c1b
christophersesugh <christohybrid185@gmail.com>
2023-02-23 15:48:08 +0000

    feat: complete 0x04 task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int number = 48;
+
+	while (number < 58)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
