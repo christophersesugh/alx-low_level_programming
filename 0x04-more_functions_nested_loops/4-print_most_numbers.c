7f74e037b6558dd1d0933c9d46cb45d809c89887
christophersesugh <christohybrid185@gmail.com>
2023-02-23 16:07:15 +0000

    feat: complete 0x04 task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0 to 9 with 2 and 4 as exceptions
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int number;
+
+	for (number = 48; number < 58; number++)
+	{
+		if (number == 50 || number == 56)
+			continue;
+		_putchar(number);
+	}
+	_putchar('\n');
+}
