cccdd3fe0556ad15f05b19f8765c64c0fa82b7b7
christophersesugh <christohybrid185@gmail.com>
2023-02-24 04:42:25 +0000

    feat: complete 0x04 task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - check for multiples of 3 and 5
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 ==0))
+		{
+			printf("FizzBuzz"):
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 ==0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
