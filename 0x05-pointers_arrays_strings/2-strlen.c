88ff22fc54142325364cdcfde5440f39c04a305f
christophersesugh <christohybrid185@gmail.com>
2023-02-27 14:29:30 +0000

    refactor: update 0x05 task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,16 +3,13 @@
 /**
  * _strlen - returns the length of a string
  * @s: string which its length is to be returned
- * Return: nothing
+ * Return: string length;
  */
 int _strlen(char *s)
 {
 	int count = 0;
 
 	while (*(s + count) != '\0')
-	{
 		count++;
-	}
-	_putchar('\n');
 	return (count);
 }
