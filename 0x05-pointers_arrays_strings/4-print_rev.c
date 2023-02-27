6d2ec2f75cb83c1e64e5ed8fcdf9bde600db7312
christophersesugh <christohybrid185@gmail.com>
2023-02-27 15:08:58 +0000

    feat: complete 0x05 task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: string to be printed in reverse
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int idx = 0;
+
+	while (s[idx] != '\0')
+	{
+		i++;
+	}
+
+	while (idx > 0)
+	{
+		idx--;
+		_putchar(s[idx]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 int _putchar(char);
 void _puts(char *str);
+void print_rev(char *s);
 
 #endif
