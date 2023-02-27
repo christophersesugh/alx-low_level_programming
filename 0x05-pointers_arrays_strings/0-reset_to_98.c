47e5eae735bde78e468d984f5b26a180a16afc72
christophersesugh <christohybrid185@gmail.com>
2023-02-27 12:10:40 +0000

    feat: complete 0x05 task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value of *n to 98
+ * @*n: the parameter of which its value is to be reset
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
