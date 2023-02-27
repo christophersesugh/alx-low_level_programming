ce874263481cdec50c1fd037ded053c81fafabfd
christophersesugh <christohybrid185@gmail.com>
2023-02-27 12:12:33 +0000

    fix: provide comment description for the parameter n in 0x05 task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - reset the value of *n to 98
- * @*n: the parameter of which its value is to be reset
+ * @n: the parameter of which its value is to be reset
  * Return: nothing
  */
 void reset_to_98(int *n)
