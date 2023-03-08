15084bb72aa421d1721e63767abe8561e2bb63d3
christophersesugh <christohybrid185@gmail.com>
2023-03-08 16:27:58 +0000

    feat: complete 0x08 task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h";
+
+/**
+ * _puts_recursion - prints a string using recursion
+ * @s: string to be printed
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*c == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*c);
+	print(c+1);
+}
