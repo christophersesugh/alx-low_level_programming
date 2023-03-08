d67eab58ca24eb4d7f6529db7213f41c5ca6930e
christophersesugh <christohybrid185@gmail.com>
2023-03-08 23:32:43 +0000

    feat: complete 0x08 task 1 and 2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string using recursion
+ * _print_rev_recursion - prints a string in reverse using recursion
  * @s: string to be printed
  * Return: nothing
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_puts_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a sstring
+ * @s: string length to be returned
+ * Return: nothing
+ */
+int _strlen_recursion(char *s)
+{
+	if(*s == '\0')
+	{
+		_putchar('\n');
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1);
+}
