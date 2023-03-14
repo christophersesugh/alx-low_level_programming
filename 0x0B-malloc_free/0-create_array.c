f4c62e87da5b4f7c290fe194cfb2dd2286e66afd
christophersesugh <christohybrid185@gmail.com>
2023-03-14 22:58:39 +0000

    fix 0x0B task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i;
 
-	s = malloc((size + 1) * sizeof(char));
+	s = malloc((size) * sizeof(char));
 	if (size == 0)
 	{
 		return (NULL);
@@ -23,7 +23,7 @@ char *create_array(unsigned int size, char c)
 	}
 
 	i = 0;
-	while(i < size)
+	while (i < size)
 	{
 		s[i] = c;
 		i++;
