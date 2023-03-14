fda91a95a19c96c2da0c64eb91e01ea333afa4fe
christophersesugh <christohybrid185@gmail.com>
2023-03-14 22:49:46 +0000

    feat: complete 0x0B task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_array - create an array of characters
+ * @size: size of the array
+ * @c: character to initialize the created array
+ * Return: pointer to the created array or NULL if failed
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	int i;
+
+	s = malloc((size + 1) * sizeof(char));
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+	while(i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+# Understnading `malloc` and `free` functions
