2593400f40e5a68a22e4abb292436f193ff736a2
christophersesugh <christohybrid185@gmail.com>
2023-03-15 00:09:00 +0000

    feat: complete 0x0B task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: the concatenated string from s1 and s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s3;
+	size_t len1, len2, i;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+
+	len1 = 0;
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	len1++;
+
+	len2 = 0;
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	len2++;
+
+	s3 = malloc((len1 + len2 + 1) * sizeof(char));
+	if (s3 == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len1; i++)
+	{
+		s3[i] = s1[i];
+	}
+	i = 0;
+	for (i = 0; i < len2; i++)
+	{
+		s3[i] = s2[i];
+	}
+	s3[i++] = '\0';
+	return (s3);
+}
