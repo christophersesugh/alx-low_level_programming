5a1882eae455d618d17ccf5009db3f2ccff71b64
christophersesugh <christohybrid185@gmail.com>
2023-03-23 23:43:16 +0000

    fix: 0x10 task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,17 +13,12 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 	va_start(args, n);
 
-	sum = 0;
 	if (n == 0)
-	{
 		return (0);
-	}
-	else
+	sum = 0;
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			sum += va_args(args, int);
-		}
+		sum += va_arg(args, int);
 	}
 	va_end(args);
 	return (sum);
