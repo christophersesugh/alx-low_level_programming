87fbe3b928b0d99f74f0df1368ae68f5453ac057
christophersesugh <christohybrid185@gmail.com>
2023-04-04 13:09:04 +0000

    fix: 0x13 task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,7 @@
 
 /**
  * listint_len - returns the number of elements in a linked list
- * h@: linked list head
+ * @h: linked list head
  * Return: the number of elements in a linked list
  */
 size_t listint_len(const listint_t *h)
