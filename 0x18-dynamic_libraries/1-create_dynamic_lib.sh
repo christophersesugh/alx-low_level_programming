1ddd27ca45212493f3a05063c81772c9f543b764
christophersesugh <christohybrid185@gmail.com>
2023-05-24 11:11:29 +0000

    fix2

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-c_files=$(find . -maxdepth 1 -type f -name '*.c')
+c_files=$(find . -maxdepth 1 -type f -name "*.c")
 
-object_files=""
-for file in $c_files; do
-    object_file="${file%.c}.o"
-    gcc -c -fPIC "$file" -o "$object_file"
-    object_files+=" $object_file"
-done
-
-gcc -shared -o liball.so $object_files
-
-rm -f *.o
+gcc -shared -o liball.so -fPIC $c_files
 
